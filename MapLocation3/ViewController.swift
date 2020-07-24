c524fc241727fbf073c4b8128ec374478cfffcef
satyam <satyam.kumar@jungleworks.com>
2020-07-24 18:45:05 +0530

    Initial Commit

---
+++ MapLocation3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapLocation3
+//
+//  Created by soc-admin on 7/24/20.
+//  Copyright © 2020 soc-admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
